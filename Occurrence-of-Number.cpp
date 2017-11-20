ab49d9edfd38e353561165d683bf8c39cd186b8e
Vishnu M Aiea <vishnumaiea@gmail.com>
2017-11-20 12:18:29 +0530

    Update and rename Occurrence of Number.cpp to Occurrence-of-Number.cpp

--- Occurrence-of-Number.cpp
+++ Occurrence-of-Number.cpp
@@ -6,21 +6,21 @@ using namespace std;
 
 int main() {
   int n, number=0, count=0, arr[50];
-  
+
   cout << "Enter total number of elements :";
   cin >> n;
   cout << "Enter " << n << " numbers :";
-  
+
   for(int i=0; i<n; i++)
     cin >> arr[i];
-  
+
   cout << "Enter number to search : ";
   cin >> number;
-  
+
   for(int i=0; i < n; i++)
-  	if(arr[i] == number) count++;
-	
-	cout << number << " appears " << count << " times.";
+    if(arr[i] == number) count++;
+
+  cout << number << " appears " << count << " times.";
   return 0;
 }
 
